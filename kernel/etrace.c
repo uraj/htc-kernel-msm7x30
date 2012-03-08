956fd5e156d57829be4e5de24726e04f35c2406a
Pei Wang <wangpei.cs@pku.edu.cn>
2012-03-08 12:25:48 +0800

    Do not log vfs operations which has nothing to do with power consumption.

--- kernel/etrace.c
+++ kernel/etrace.c
@@ -2,6 +2,7 @@
 #include <linux/kprobes.h>
 #include <linux/kernel.h>
 #include <linux/elogk.h>
+#include <linux/string.h>
 
 /*
  * Our key idea: list every important syscall and instrument their
@@ -30,15 +31,33 @@
 #define STR_BUF_SIZE          256
 #define EBUF_SIZE             (STR_BUF_SIZE + sizeof(struct eevent_t))
 #define SBUF_SIZE             32
+#define ERR_FILENAME          ("(.)")
 
 static char __buf[STR_BUF_SIZE];
-
+    
 /* needed by hooking open, read, write */
 #include <linux/fs.h>
+
+struct fs_kretprobe_data
+{
+    __s16 id;
+    __u16 hook_ret;
+};
 
 #define EEVENT_OPEN_NO       1
 #define EEVENT_READ_NO       2
 #define EEVENT_WRITE_NO      3
+
+static inline int file_filter(char * path, size_t length)
+{
+    if (length < 5)
+        return 0;
+    
+    if (strncmp(path, "/proc", 5) == 0)
+        return 1;
+    
+    return 0;
+}
 
 /*
  * hooked func:
@@ -53,21 +72,18 @@ static void open_handler(unsigned int fd, struct file *file)
     
     struct eevent_t *eevent = (struct eevent_t *)buf;
     
+    fpath = d_path(&file->f_path, __buf, STR_BUF_SIZE);
+    if (IS_ERR(fpath))
+        fpath = ERR_FILENAME;
+    fpath_len = strlen(fpath);
+
+    if (file_filter(fpath, fpath_len))
+        jprobe_return();
+
     eevent->syscall_no = EEVENT_OPEN_NO;
     eevent->id = id++;
-
-    fpath = d_path(&file->f_path, __buf, STR_BUF_SIZE);
-    if(IS_ERR(fpath))
-    {
-        fpath = "(...)";
-        fpath_len = strlen(fpath);
-    }
-    else
-        fpath_len = strlen(fpath);
-    
     eevent->len =
-        sprintf(eevent->params, "%u,\"%.*s\"",
-                fd, fpath_len, fpath);
+        sprintf(eevent->params, "%u,\"%.*s\"", fd, fpath_len, fpath);
 
     elogk(eevent);
 
@@ -100,23 +116,26 @@ static int read_entry_handler(struct kretprobe_instance *ri, struct pt_regs *reg
     
     char *fpath;
     int fpath_len;
+
+    struct fs_kretprobe_data *data = (struct fs_kretprobe_data *)ri->data;
     
-    *((__s16 *)ri->data) = id;
+    fpath = d_path(&arg0->f_path, __buf, STR_BUF_SIZE);
+    if (IS_ERR(fpath))
+        fpath = ERR_FILENAME;
+    fpath_len = strlen(fpath);
+    
+    if (file_filter(fpath, fpath_len))
+    {
+        data->hook_ret = 0;
+        return 0;
+    }
+
+    data->hook_ret = 1;
+    data->id = id;
     eevent->syscall_no = EEVENT_READ_NO;
     eevent->id = id++;
-
-    fpath = d_path(&arg0->f_path, __buf, STR_BUF_SIZE);
-    if(IS_ERR(fpath))
-    {
-        fpath = "(...)";
-        fpath_len = strlen(fpath);
-    }
-    else
-        fpath_len = strlen(fpath);
-    
-    eevent->len =
-        sprintf(eevent->params, "\"%.*s\",%p,%u,%p",
-                fpath_len, fpath, arg1, arg2, arg3);
+    eevent->len = sprintf(eevent->params, "\"%.*s\",%p,%u,%p",
+                          fpath_len, fpath, arg1, arg2, arg3);
     
     elogk(eevent);
     
@@ -126,9 +145,15 @@ static int read_entry_handler(struct kretprobe_instance *ri, struct pt_regs *reg
 static int read_ret_handler(struct kretprobe_instance *ri, struct pt_regs *regs)
 {
     static char buf[SBUF_SIZE];
-    struct eevent_t *eevent = (struct eevent_t *)buf;
+    struct eevent_t *eevent;
 
+    struct fs_kretprobe_data *data = (struct fs_kretprobe_data *)ri->data;
+
+    if (data->hook_ret == 0)
+        return 0;
+    
     /* negative id for return entry */
+    eevent = (struct eevent_t *)buf;
     eevent->id = -*((__s16 *)ri->data);
     eevent->syscall_no = EEVENT_READ_NO;
     eevent->len = sprintf(eevent->params, "%d", (ssize_t)regs_return_value(regs));
@@ -142,7 +167,7 @@ static struct kretprobe read_kretprobe =
 {
     .handler = read_ret_handler,
     .entry_handler = read_entry_handler,
-    .data_size = sizeof(__s16),
+    .data_size = sizeof(struct fs_kretprobe_data),
     .kp =
     {
         .symbol_name = "vfs_read",
@@ -167,20 +192,24 @@ static int write_entry_handler(struct kretprobe_instance *ri, struct pt_regs *re
 
     char *fpath;
     int fpath_len;
-    
-    *((__s16 *)ri->data) = id;
-    eevent->syscall_no = EEVENT_WRITE_NO;
-    eevent->id = id++;
 
-    fpath = d_path(&arg0->f_path, __buf, STR_BUF_SIZE);
-    if(IS_ERR(fpath))
-    {
-        fpath = "(...)";
-        fpath_len = strlen(fpath);
-    }
-    else
-        fpath_len = strlen(fpath);
+    struct fs_kretprobe_data *data = (struct fs_kretprobe_data *)ri->data;
     
+    fpath = d_path(&arg0->f_path, __buf, STR_BUF_SIZE);
+    if (IS_ERR(fpath))
+        fpath = ERR_FILENAME;
+    fpath_len = strlen(fpath);
+    
+    if (file_filter(fpath, fpath_len))
+    {
+        data->hook_ret = 0;
+        return 0;
+    }
+    
+    data->hook_ret = 1;
+    data->id = id;
+    eevent->syscall_no = EEVENT_WRITE_NO;
+    eevent->id = id++;    
     eevent->len =
         sprintf(eevent->params, "\"%.*s\",%p,%u,%p",
                 fpath_len, fpath, arg1, arg2, arg3);
@@ -193,10 +222,15 @@ static int write_entry_handler(struct kretprobe_instance *ri, struct pt_regs *re
 static int write_ret_handler(struct kretprobe_instance *ri, struct pt_regs *regs)
 {
     static char buf[SBUF_SIZE];
-    struct eevent_t *eevent = (struct eevent_t *)buf;
+    struct eevent_t *eevent;
+    struct fs_kretprobe_data *data = (struct fs_kretprobe_data *)ri->data;
 
+    if (data->hook_ret == 0)
+        return 0;
+    
     /* negative id for return entry */
-    eevent->id = -*((__s16 *)ri->data);
+    eevent = (struct eevent_t *)buf;
+    eevent->id = - data->id;
     eevent->syscall_no = EEVENT_WRITE_NO;
     eevent->len = sprintf(eevent->params, "%d", (ssize_t)regs_return_value(regs));
     
@@ -209,7 +243,7 @@ static struct kretprobe write_kretprobe =
 {
     .handler = write_ret_handler,
     .entry_handler = write_entry_handler,
-    .data_size = sizeof(__s16),
+    .data_size = sizeof(struct fs_kretprobe_data),
     .kp =
     {
         .symbol_name = "vfs_write",
